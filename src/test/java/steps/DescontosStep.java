d81ff94f8c87d2cf9595484c1635d557aa879049
Odair <opanciera@hotamil.com>
2023-04-20 20:03:02 -0300

    Criando as primeiras class e preparando ambiente. Criando class descontosSteps e preparando os primeiros metodos para os testes. Criando desconto.feature e fazendo os primeiros cenarios.
    criado o primeiro metodo chamando no metodo o WebDriver
    
    implementando os Assert para fazer validacoes

--- src/test/java/steps/DescontosStep.java
+++ src/test/java/steps/DescontosStep.java
@@ -3,6 +3,9 @@ package steps;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -16,14 +19,26 @@ public class DescontosStep {
     }
 
     @Quando("^eu preencho meu e-mail$")
-    public void eu_preencho_meu_e_mail() {
+    public void eu_preencho_meu_e_mail() throws InterruptedException {
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("window.scrollTo(0, 10000)");
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("email")).sendKeys("odairqatest@test.com.br");
     }
 
     @Quando("^clico em ganhar cupom$")
     public void clico_em_ganhar_cupom() {
+        driver.findElement(By.id("button")).click();
     }
 
     @Entao("^eu vejo o codigo de desconto$")
     public void eu_vejo_o_codigo_de_desconto() {
+        String texto_cupom = driver.findElement(By.cssSelector("#cupom > h2 > span")).getText();
+        Assert.assertEquals("O cupom está errado!", "QAZANDO15OFF", texto_cupom);
+        // esse Assert é para verificar se realmente estamos no site correto
+
+        driver.quit();
     }
+
 }
