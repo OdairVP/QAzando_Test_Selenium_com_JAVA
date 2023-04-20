e7bd28e7665af7954e0cd4f02f2a1af6e75219f9
Odair <opanciera@hotamil.com>
2023-04-20 18:34:20 -0300

    Criando as primeiras class e preparando ambiente. Criando class descontosSteps e preparando os primeiros metodos para os testes. Criando desconto.feature e fazendo os primeiros cenarios.
    criado o primeiro metodo chamando no metodo o WebDriver

--- src/test/java/steps/DescontosStep.java
+++ src/test/java/steps/DescontosStep.java
@@ -3,26 +3,27 @@ package steps;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DescontosStep {
 
+    WebDriver driver = new ChromeDriver();
+
     @Dado("^que estou no site da qazando$")
     public void que_estou_no_site_da_qazando() {
-        System.out.println("passou 01");
+        driver.get("https://qazando.com.br/curso.html");
     }
 
     @Quando("^eu preencho meu e-mail$")
     public void eu_preencho_meu_e_mail() {
-        System.out.println("passou 02");
     }
 
     @Quando("^clico em ganhar cupom$")
     public void clico_em_ganhar_cupom() {
-        System.out.println("passou 03");
     }
 
     @Entao("^eu vejo o codigo de desconto$")
     public void eu_vejo_o_codigo_de_desconto() {
-        System.out.println("passou 04");
     }
 }
