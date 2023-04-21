d8bb67590542e802e92b15cfdf98261f40628fb0
Odair <opanciera@hotamil.com>
2023-04-20 21:57:36 -0300

    Criando as primeiras class e preparando ambiente. Criando class descontosSteps e preparando os primeiros metodos para os testes. Criando desconto.feature e fazendo os primeiros cenarios.
    criado o primeiro metodo chamando no metodo o WebDriver
    
    implementando os Assert para fazer validacoes
    
    comecando a organizar o codigo, abri class HomePage e ja foram feitas as organizacoes dos codigos, entre test e metodos.
    
    Acabei de inserir o README.md
    
    Criado a class Utils dentro do diretorio support, para criarmos as ferramentas a serem usadas nos testes
    
    foi aplicado a ferramenta wait no metodo acessarAplicacao() e tbm feito a verificação.
    
    no metodo verificarCupomDesconto está apresentando inconsistencia, e levando ao erro.

--- src/test/java/page/HomePage.java
+++ src/test/java/page/HomePage.java
@@ -1,10 +1,12 @@
 package page;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import support.Utils;
 
-public class HomePage {
+public class HomePage extends Utils {
 
     WebDriver driver;
 
@@ -14,9 +16,14 @@ public class HomePage {
 
     public void acessarAplicacao(){
         driver.get("https://qazando.com.br/curso.html");
+        waitEsperarElementoEstarPresente(By.id("btn-ver-cursos"),10); // Essa é uma forma correta de esperar um elemento estar presente na tela.
+        Assert.assertEquals("Não acessou a aplicação", true, driver.findElement(By.id("btn-ver-cursos")).isDisplayed());
     }
 
     public void scrollDown() throws InterruptedException {
+        // Essa é uma forma errada de esperar um elemento estar presente na tela, ele pode ser usado momentaneamente, mas depois devemos encontrar a ...
+        // ... melhor forma de resolver esse impasse.  nesse caso o instrutor passou que mesmo "errado" acaba sendo a melhor forma de fazer.
+
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         jse.executeScript("window.scrollTo(0, 10000)");
         Thread.sleep(2000);
@@ -31,8 +38,7 @@ public class HomePage {
 
     public void verificarCupomDesconto(){
         String texto_cupom = driver.findElement(By.cssSelector("#cupom > h2 > span")).getText();
-//        Assert.assertEquals("O cupom está errado!", "QAZANDO15OFF", texto_cupom);
-        // esse Assert é para verificar se realmente estamos no site correto
+//        Assert.assertEquals("O cupom está errado!", " QAZANDO15OFF", texto_cupom);
+//         esse Assert é para verificar se realmente o cupom está correto, mas está dando ERRO!
     }
-
 }
--- src/test/java/support/Utils.java
+++ src/test/java/support/Utils.java
@@ -1,4 +1,15 @@
 package support;
 
-public class Utils {
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import runner.RunCucumberTest;
+
+public class Utils extends RunCucumberTest {
+
+    public void waitEsperarElementoEstarPresente(By element, int tempo){
+        WebDriverWait wait= new WebDriverWait(driver, tempo);
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
 }
